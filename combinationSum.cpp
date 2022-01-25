b153efce7862462a68c685a383cb2a4882523f50
Yaswanth krishna Kondapaneni <68105563+yash-chintu@users.noreply.github.com>
2022-01-25 13:32:03 +0530

    Create combinationSum.cpp

---
+++ combinationSum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    map<vector<int>,bool>ans;
+    void compute(vector<int> c, int target,int val,vector<int> so_far){
+    for(int i=0;i<c.size();i++){
+        if(val+c[i]<target){
+            vector<int> temp=so_far;
+            temp.push_back(c[i]);
+            compute(c,target,val+c[i],temp);
+        }
+        else if(val+c[i]==target){
+            vector<int> temp=so_far;
+            temp.push_back(c[i]);
+            sort(temp.begin(),temp.end());
+            ans[temp]=true;
+        }
+        else{
+            return ;    
+        }
+    }
+        return ;
+    }
+    vector<vector<int>> combinationSum(vector<int>& c, int target) {
+        sort(c.begin(),c.end());
+        vector<int> empty;
+        vector<vector<int>>Ans;
+        compute(c,target,0,empty);
+        for(auto x :ans){
+            Ans.push_back(x.first);
+        }
+        return Ans;
+    }
+};
