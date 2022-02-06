922632e808faa61d242bc747c8a472cac7b03a70
unknown <krishnayaswanth4@gmail.com>
2022-02-06 12:19:56 +0530

    day 6 done

---
+++ Feb2022/day6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+     int k=nums.size();
+        bool prev=false;
+        for(int i=1;i<k;i++){
+            if(nums[i]==nums[i-1]){
+                if(prev){
+                    nums.erase(nums.begin()+i);
+                    k--;
+                    i--;
+                }
+                else{
+                    prev=true;
+                }
+            }
+            else{
+                prev=false;
+            }
+        }
+        return k;
+    }
+};
